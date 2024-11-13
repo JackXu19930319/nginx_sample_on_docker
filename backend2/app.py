d1874914df0420cf773ff7f8407270c43ecc61e0
JackXu19930319 <49975224+JackXu19930319@users.noreply.github.com>
2024-11-13 11:46:38 +0800

    多台服務在同一台機器上運作

---
+++ backend2/app.py
@@ -0,0 +1,16 @@
+import os
+
+from flask import Flask
+
+work_port = int(os.environ.get("PORT", '8001'))
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return f'Hello, World {work_port}!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=work_port)
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
-# 如何使用nginx實現輪詢＋保持通話功能
+# 在同一台機器上部署不同網站
+
+1. docker network create work_net
+2. docker network connect work_net jack_test_app
